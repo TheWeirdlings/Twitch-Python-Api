d3dc4be377b0ee46298b38bb298c69610259df19
Keith Holliday <krh121791@gmail.com>
2017-05-17 13:31:32 -0600

    Added stream api and some channel routes

--- twitch_api.py
+++ twitch_api.py
@@ -2,9 +2,25 @@ import requests
 import json
 import config
 
+class StreamApi:
+    def __init__(self, rootApiUrl):
+        self.rootApiUrl = rootApiUrl
+
+    def getStreams(self, channelId):
+        url = self.rootApiUrl + "streams/%s" % channelId
+        queryString = ""
+
+        headers = {
+            'Client-ID': config.twitchClientID
+        }
+
+        req = requests.request('GET', url + queryString, headers=headers)
+        return req.text
+
 class TwitchApi:
     def __init__(self):
         self.rootApiUrl = "https://api.twitch.tv/kraken/"
+        self.streams = StreamApi(self.rootApiUrl)
 
     def getFollowers(self, channel = None, cursor = None, limit = 25, direction = 'desc'):
         if channel is None:
@@ -31,9 +47,31 @@ class TwitchApi:
         req = requests.request('GET', url + queryString)
         return req.text
 
+    def getChannel(self):
+        url = self.rootApiUrl + "channel"
+        queryString = ""
+
+        headers = {
+            'Client-ID': config.twitchClientID
+        }
+        print(headers)
+        req = requests.request('GET', url + queryString, headers=headers)
+        return req.text
+
+    def getChannelById(self, channelId):
+        url = self.rootApiUrl + "channels/%s" % channelId
+        queryString = ""
+
+        headers = {
+            'Client-ID': config.twitchClientID
+        }
+
+        req = requests.request('GET', url + queryString, headers=headers)
+        return req.text
+
 if __name__ == "__main__":
     twitchApi = TwitchApi()
-    followers = twitchApi.getFollowers('weirdlings')
+    followers = twitchApi.streams.getStreams('thehollidayinn')
     followers = json.loads(followers)
     print(followers)
     print(followers['_cursor'])
